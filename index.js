9c25c02be725aa543afceef47186e5bc866a6e02
unibreakfast <unibreakfast1@gmail.com>
2019-12-16 11:55:35 +0200

    fsDirect added

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
 
-const
-fs = require('fs'),
-jsonClone = obj => JSON.parse(JSON.stringify(obj)),
-
-read = queryFn => {
-  const backup = jsonClone(store),
-        result = queryFn(store)
-  if (JSON.stringify(store) == JSON.stringify(backup))
-    return jsonClone(result)
-  store = backup
-  throw new Error('Only read query operations allowed!')
-},
-
-update = queryFn => {
-  const backup = jsonClone(store)
-  try {
-    const result = queryFn(store)
-    fs.writeFileSync('store.json', JSON.stringify(store, ' ', 2))
-    return jsonClone(result)
-  }
-  catch {
-    store = backup
-    throw new Error('New store state must be JSON-stringifiable!')
-  }
-}
-
-let store = JSON.parse(fs.readFileSync('store.json', 'utf8') || '{}')
-
-// module.exports = {read, update}
-module.exports = require('./store/fsDirectSync')
+module.exports = require(
+  './store/fsDirect'
+  // './store/fsDirectSync'
+  // './store/inMem'
+  // './store/inMemSync'
+)
--- store/fsDirect.js
+++ store/fsDirect.js
@@ -1,30 +1,22 @@
 
 const
 fsp = require('fs').promises,
-jsonClone = obj => JSON.parse(JSON.stringify(obj)),
 
-read = async queryFn => {
-  const backup = jsonClone(store),
-        result = await queryFn(store)
-  if (JSON.stringify(store) == JSON.stringify(backup))
-    return jsonClone(result)
-  store = backup
-  throw new Error('Only read query operations allowed!')
-},
+read = async queryFn =>
+  await queryFn(JSON.parse(await fsp.readFile('store.json', 'utf8') || '{}')),
 
 update = async queryFn => {
-  const backup = jsonClone(store)
+  const file = await fsp.readFile('store.json', 'utf8') || '{}',
+        store = JSON.parse(file)
   try {
-    const result = await queryFn(store)
-    await fsp.writeFile('store.json', JSON.stringify(store, ' ', 2))
-    return jsonClone(result)
+    const result = await queryFn(store),
+          json = JSON.stringify(store, ' ', 2)
+    if (file != json) await fsp.writeFile('store.json', json)
+    return result
   }
   catch {
-    store = backup
     throw new Error('New store state must be JSON-stringifiable!')
   }
 }
-
-let store = JSON.parse(require('fs').readFileSync('store.json', 'utf8') || '{}')
 
 module.exports = {read, update}
